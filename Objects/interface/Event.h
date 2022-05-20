bacf93ff8a88d6afe7da0b933637726e1d524f49
Christian Veelken <christian.veelken@cern.ch>
2022-05-20 18:09:18 +0300

    synchronization with old software. The only remaining differences in HH->multilepton 2lss+1tau channel are: - old software selects event in case at least one tau passes tight cut. The new software first selects the leading fakeable tau and demands that this tau passes the tight cut (events in which the leading fakeable tau fails the tight cut, but the event contains another tight tau are rejected by the new software) - a few events fail in the new software due to the HLT trigger bit matching to leptons and taus (HLT trigger bit matching is not applied in the old software)

--- Objects/interface/Event.h
+++ Objects/interface/Event.h
@@ -63,6 +63,8 @@ class Event
   bool isInvalid() const;
 
   friend class EventReader;
+  friend class RecoHadTauMultiplicityWriter;
+  friend class RecoLeptonMultiplicityWriter;
 
  protected:
   const EventInfo * eventInfo_;
