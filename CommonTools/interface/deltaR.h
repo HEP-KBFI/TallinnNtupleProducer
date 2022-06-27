3a8a18acf5277ecb256d1487e290d62fd6cb4b49
saswatinandan <saswati.nandan@cern.ch>
2022-06-27 14:44:15 +0300

    removing deltaR function

--- CommonTools/interface/deltaR.h
+++
@@ -1,7 +0,0 @@
-#ifndef TallinnNtupleProducer_deltaR_h
-#define TallinnNtupleProducer_deltaR_h
-
-double
-deltaR(double eta1, double eta2, double phi1, double phi2);
-
-#endif
--- CommonTools/src/deltaR.cc
+++
@@ -1,8 +0,0 @@
-#include "TallinnNtupleProducer/CommonTools/interface/deltaR.h"
-#include "TMath.h"
-
-double
-deltaR(double eta1, double eta2, double phi1, double phi2)
-{
-  return TMath::Sqrt( pow( (eta1 - eta2), 2 ) + pow( (phi1 - phi2), 2 ) );
-}
