5bf99780aa1d27a8a6ba42183d51cd7d9ea3011d
Christian Veelken <christian.veelken@cern.ch>
2022-06-28 10:53:46 +0300

    restructuring of code for trigger object matching

---
+++ Readers/interface/setFilterBits.h
@@ -0,0 +1,35 @@
+#ifndef TallinnNtupleProducer_Readers_setFilterBits_h
+#define TallinnNtupleProducer_Readers_setFilterBits_h
+
+#include "TallinnNtupleProducer/Objects/interface/TriggerInfo.h" // TriggerInfo
+
+#include <DataFormats/Math/interface/deltaR.h>                   // deltaR()
+
+#include <assert.h>                                              // assert()
+#include <vector>                                                // std::vector
+
+template <typename T>
+void
+setFilterBits(std::vector<T> & particles, const TriggerInfo& triggerInfo)
+{
+  const std::vector<trigger::Object> & triggerObjects = triggerInfo.objects();
+  for ( T & particle : particles )
+  {
+    const T * particlePtr = &particle;
+    Int_t filterBits = 0;
+    for ( const trigger::Object & triggerObject : triggerObjects )
+    {
+      if ( (triggerObject.is_electron() && dynamic_cast<const RecoElectron *>(particlePtr)) ||
+           (triggerObject.is_muon()     && dynamic_cast<const RecoMuon *>(particlePtr)    ) ||
+           (triggerObject.is_hadTau()   && dynamic_cast<const RecoHadTau *>(particlePtr)  ) ) {
+        if ( deltaR(particle.eta(), particle.phi(), triggerObject.eta(), triggerObject.phi()) < 0.05 )
+        {
+          filterBits |= triggerObject.filterBits();
+        }
+      }
+    }
+    particle.set_filterBits(filterBits);
+  }
+}
+
+#endif // TallinnNtupleProducer_Readers_setFilterBits_h
