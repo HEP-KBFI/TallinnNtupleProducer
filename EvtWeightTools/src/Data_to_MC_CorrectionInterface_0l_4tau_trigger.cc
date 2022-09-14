be4ecb4513a5f8ef0e8bdd4c3c53e02e02275d34
saswatinandan <saswati.nandan@cern.ch>
2022-09-14 09:10:04 +0300

    Revert "use trigger matched hadTau to calculate sf"
    This reverts commit 711f430e231560d71542b735d6d5f34844ab0447.

--- EvtWeightTools/src/Data_to_MC_CorrectionInterface_0l_4tau_trigger.cc
+++ EvtWeightTools/src/Data_to_MC_CorrectionInterface_0l_4tau_trigger.cc
@@ -1,10 +1,8 @@
-#include <DataFormats/Math/interface/deltaR.h>                        // deltaR()
-#include "TallinnNtupleProducer/Objects/interface/RecoHadTau.h"       // RecoHadTau::filterBits()
 #include "TallinnNtupleProducer/EvtWeightTools/interface/Data_to_MC_CorrectionInterface_0l_4tau_trigger.h"
-#include "TallinnNtupleProducer/EvtWeightTools/interface/data_to_MC_corrections_auxFunctions.h" // aux::
 
 #include "TallinnNtupleProducer/CommonTools/interface/cmsException.h"                           // cmsException()
 #include "TallinnNtupleProducer/CommonTools/interface/sysUncertOptions.h"                       // TriggerSFsys, getTriggerSF_option()
+#include "TallinnNtupleProducer/EvtWeightTools/interface/data_to_MC_corrections_auxFunctions.h" // aux::
 
 #include <TString.h> // Form()
 
@@ -42,27 +40,115 @@ Data_to_MC_CorrectionInterface_0l_4tau_trigger::getSF_triggerEff(TriggerSFsys ce
     sys = "down";
   }
 
-  std::vector<const RecoHadTau*> matched_hadTaus;
+  double eff_2tau_tauLeg1_data = 0.;
+  double eff_2tau_tauLeg1_mc   = 0.;
+  double eff_2tau_tauLeg2_data = 0.;
+  double eff_2tau_tauLeg2_mc   = 0.;
+  double eff_2tau_tauLeg3_data = 0.;
+  double eff_2tau_tauLeg3_mc   = 0.;
+  double eff_2tau_tauLeg4_data = 0.;
+  double eff_2tau_tauLeg4_mc   = 0.;
 
-  for (auto hadTau_recObj : hadTau_recObjs)
+  if(std::fabs(hadTau1_eta_) <= 2.1 && aux::hasDecayMode(allowedDecayModes_, hadTau1_decayMode_))
   {
-    //can we use 64, 128 filterbits only as they are related to di-tau trigger
-    //https://cmssdt.cern.ch/lxr/source/PhysicsTools/NanoAOD/python/triggerObjects_cff.py?v=CMSSW_12_6_X_2022-09-08-2300 #Line-0122
-    if ( hadTau_recObj->filterBits() ) matched_hadTaus.push_back(hadTau_recObj);
+    eff_2tau_tauLeg1_data = tau_leg_efficiency(hadTau1_pt_, hadTau1_decayMode_, "ditau", wp_str_, "eff_data", sys);
+    eff_2tau_tauLeg1_mc   = tau_leg_efficiency(hadTau1_pt_, hadTau1_decayMode_, "ditau", wp_str_, "eff_mc", sys);
   }
 
-  double prob_data(1);
-  double prob_mc(1);
-  for( auto matched_hadTau : matched_hadTaus)
+  if(std::fabs(hadTau2_eta_) <= 2.1 && aux::hasDecayMode(allowedDecayModes_, hadTau2_decayMode_))
   {
-    prob_data *= tau_leg_efficiency(matched_hadTau->pt(), matched_hadTau->decayMode(), "ditau", wp_str_, "eff_data", sys);
-    prob_mc *= tau_leg_efficiency(matched_hadTau->pt(), matched_hadTau->decayMode(), "ditau", wp_str_, "eff_mc", sys);
+    eff_2tau_tauLeg2_data = tau_leg_efficiency(hadTau2_pt_, hadTau2_decayMode_, "ditau", wp_str_, "eff_data", sys);
+    eff_2tau_tauLeg2_mc   = tau_leg_efficiency(hadTau2_pt_, hadTau2_decayMode_, "ditau", wp_str_, "eff_mc", sys);
   }
 
-  if ( isDEBUG_ )
-    std::cout << "prob_data: " << prob_data << "\t prob_mc: " << prob_mc << std::endl;
+  if(std::fabs(hadTau3_eta_) <= 2.1 && aux::hasDecayMode(allowedDecayModes_, hadTau3_decayMode_))
+  {
+    eff_2tau_tauLeg3_data = tau_leg_efficiency(hadTau3_pt_, hadTau2_decayMode_, "ditau", wp_str_, "eff_data", sys);
+    eff_2tau_tauLeg3_mc   = tau_leg_efficiency(hadTau3_pt_, hadTau3_decayMode_, "ditau", wp_str_, "eff_mc", sys);
+  }
+
+  if(std::fabs(hadTau4_eta_) <= 2.1 && aux::hasDecayMode(allowedDecayModes_, hadTau4_decayMode_))
+  {
+    eff_2tau_tauLeg4_data = tau_leg_efficiency(hadTau4_pt_, hadTau4_decayMode_, "ditau", wp_str_, "eff_data", sys);
+    eff_2tau_tauLeg4_mc   = tau_leg_efficiency(hadTau4_pt_, hadTau4_decayMode_, "ditau", wp_str_, "eff_mc", sys);
+  }
+
+  double prob_data = 0.;
+  double prob_mc   = 0.;
+
+  for(int tau1_status = k2tau; tau1_status <= kNot2tau; ++tau1_status)
+  {
+    const double prob_tau1_data = getProb_tau(tau1_status, eff_2tau_tauLeg1_data);
+    const double prob_tau1_mc   = getProb_tau(tau1_status, eff_2tau_tauLeg1_mc);
+
+    for(int tau2_status = k2tau; tau2_status <= kNot2tau; ++tau2_status)
+    {
+      const double prob_tau2_data = getProb_tau(tau2_status, eff_2tau_tauLeg2_data);
+      const double prob_tau2_mc   = getProb_tau(tau2_status, eff_2tau_tauLeg2_mc);
+
+      for(int tau3_status = k2tau; tau3_status <= kNot2tau; ++tau3_status)
+      {
+        const double prob_tau3_data = getProb_tau(tau3_status, eff_2tau_tauLeg3_data);
+        const double prob_tau3_mc   = getProb_tau(tau3_status, eff_2tau_tauLeg3_mc);
+
+        for(int tau4_status = k2tau; tau4_status <= kNot2tau; ++tau4_status)
+        {
+          const double prob_tau4_data = getProb_tau(tau4_status, eff_2tau_tauLeg4_data);
+          const double prob_tau4_mc   = getProb_tau(tau4_status, eff_2tau_tauLeg4_mc);
+
+          int nTrig_2tau_tauLeg = 0;
+          if(tau1_status == k2tau)
+          {
+            ++nTrig_2tau_tauLeg;
+          }
+          if(tau2_status == k2tau)
+          {
+            ++nTrig_2tau_tauLeg;
+          }
+          if(tau3_status == k2tau)
+          {
+            ++nTrig_2tau_tauLeg;
+          }
+          if(tau4_status == k2tau)
+          {
+            ++nTrig_2tau_tauLeg;
+          }
+
+          const bool isTrig_2tau_toy = nTrig_2tau_tauLeg >= 2;
+
+          if(isTriggered_2tau_ == isTrig_2tau_toy)
+          {
+            prob_data += prob_tau1_data * prob_tau2_data * prob_tau3_data * prob_tau4_data;
+            prob_mc   += prob_tau1_mc   * prob_tau2_mc   * prob_tau3_mc   * prob_tau4_mc;
+          }
+        }
+      }
+    }
+  }
+
+  if(isDEBUG_)
+  {
+    std::cout << "hadTau (lead):    pT = " << hadTau1_pt_ << ", eta = " << hadTau1_eta_ << "\n"
+                 "hadTau (sublead): pT = " << hadTau2_pt_ << ", eta = " << hadTau2_eta_ << "\n"
+                 "hadTau (third):   pT = " << hadTau3_pt_ << ", eta = " << hadTau3_eta_ << "\n"
+                 "hadTau (fourth):  pT = " << hadTau4_pt_ << ", eta = " << hadTau4_eta_ << "\n"
+                 "eff (X_tau1): data = " << eff_2tau_tauLeg1_data   << ", MC = " << eff_2tau_tauLeg1_mc   << "\n"
+                 "eff (X_tau2): data = " << eff_2tau_tauLeg2_data   << ", MC = " << eff_2tau_tauLeg2_mc   << "\n"
+                 "eff (X_tau3): data = " << eff_2tau_tauLeg3_data   << ", MC = " << eff_2tau_tauLeg3_mc   << "\n"
+                 "eff (X_tau4): data = " << eff_2tau_tauLeg4_data   << ", MC = " << eff_2tau_tauLeg4_mc   << '\n'
+    ;
+  }
 
   const double sf = aux::compSF(prob_data, prob_mc);
+  if(isDEBUG_)
+  {
+    const int idxCase = isTriggered_2tau_ ? 1 : 0;
+    const std::string text_2tau = isTriggered_2tau_ ? "ditau trigger doesn't fire" : "ditau trigger fires";
+    std::cout << "case " << idxCase << ": " << text_2tau << "\n"
+                 " eff: data = " << prob_data << ", MC = " << prob_mc << " --> SF = " << sf << '\n'
+    ;
+  }
+
   return sf;
 }
  
